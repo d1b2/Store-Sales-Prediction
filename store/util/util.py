fdbfc8bb54f7401de91277817086cdbb1356a8de
Dhruv Bakshi <dhruvbakshi20@gmail.com>
2022-08-04 00:11:41 +0530

    added configuration.py and util.py files

---
+++ store/util/util.py
@@ -0,0 +1,14 @@
+import yaml
+from store.exception import StoreException
+import os,sys
+
+def read_yaml_file(file_path:str)->dict:
+    """
+    Reads a YAML file and returns the contents as a dictionary.
+    file_path: str
+    """
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise StoreException(e,sys) from e
