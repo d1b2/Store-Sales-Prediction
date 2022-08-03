9f0dbb0e545f9f335b00bfd56a89606443ae1415
Dhruv Bakshi <dhruvbakshi20@gmail.com>
2022-08-03 15:12:53 +0530

    Exception and logger completed

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from store.logger import logging
+from store.exception import StoreException
 
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    logging.info("Testing logging module")
-    return "CI/CD pipeline established"
-
+    try:
+        raise Exception("Testing custom exception")
+    except Exception as e:
+        store = StoreException(e,sys)
+        logging.info(store.error_message)
+        logging.info("Testing logging module")
+    return "CI CD pipeline has been established."
 
 if __name__=="__main__":
     app.run(debug=True)
+
+
+
+
